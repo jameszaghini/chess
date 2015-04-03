a253b68ec604fb6393f1d7d2675d0de41e929df5
James Zaghini <james@zaghini.com>
2015-04-03 20:52:31 +1000

    remove rt shader system

--- chess/Chess.cpp
+++ chess/Chess.cpp
@@ -60,126 +60,21 @@ Chess::Chess()
 
 Chess::~Chess()
 {
-#ifdef INCLUDE_RTSHADER_SYSTEM
-    mShaderGenerator->removeSceneManager(OgreFramework::getSingletonPtr()->m_pSceneMgr);
-    
-    destroyRTShaderSystem();
-#endif
-    
     delete OgreFramework::getSingletonPtr();
 }
-
-#ifdef INCLUDE_RTSHADER_SYSTEM
-
-/*-----------------------------------------------------------------------------
- | Initialize the RT Shader system.	
- -----------------------------------------------------------------------------*/
-bool Chess::initialiseRTShaderSystem(Ogre::SceneManager* sceneMgr)
-{			
-    if (Ogre::RTShader::ShaderGenerator::initialize())
-    {
-        mShaderGenerator = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
-        
-        mShaderGenerator->addSceneManager(sceneMgr);
-        
-        // Setup core libraries and shader cache path.
-        Ogre::StringVector groupVector = Ogre::ResourceGroupManager::getSingleton().getResourceGroups();
-        Ogre::StringVector::iterator itGroup = groupVector.begin();
-        Ogre::StringVector::iterator itGroupEnd = groupVector.end();
-        Ogre::String shaderCoreLibsPath;
-        Ogre::String shaderCachePath;
-        
-        for (; itGroup != itGroupEnd; ++itGroup)
-        {
-            Ogre::ResourceGroupManager::LocationList resLocationsList = Ogre::ResourceGroupManager::getSingleton().getResourceLocationList(*itGroup);
-            Ogre::ResourceGroupManager::LocationList::iterator it = resLocationsList.begin();
-            Ogre::ResourceGroupManager::LocationList::iterator itEnd = resLocationsList.end();
-            bool coreLibsFound = false;
-            
-            // Try to find the location of the core shader lib functions and use it
-            // as shader cache path as well - this will reduce the number of generated files
-            // when running from different directories.
-            for (; it != itEnd; ++it)
-            {
-                if ((*it)->archive->getName().find("RTShaderLib") != Ogre::String::npos)
-                {
-                    shaderCoreLibsPath = (*it)->archive->getName() + "/";
-                    shaderCachePath = shaderCoreLibsPath;
-                    coreLibsFound = true;
-                    break;
-                }
-            }
-            // Core libs path found in the current group.
-            if (coreLibsFound) 
-                break; 
-        }
-        
-        // Core shader libs not found -> shader generating will fail.
-        if (shaderCoreLibsPath.empty())			
-            return false;			
-        
-        // Create and register the material manager listener.
-        mMaterialMgrListener = new ShaderGeneratorTechniqueResolverListener(mShaderGenerator);				
-        Ogre::MaterialManager::getSingleton().addListener(mMaterialMgrListener);
-    }
-    
-    return true;
-}
-
-/*-----------------------------------------------------------------------------
- | Destroy the RT Shader system.
- -----------------------------------------------------------------------------*/
-void Chess::destroyRTShaderSystem()
-{
-    // Restore default scheme.
-    Ogre::MaterialManager::getSingleton().setActiveScheme(Ogre::MaterialManager::DEFAULT_SCHEME_NAME);
-    
-    // Unregister the material manager listener.
-    if (mMaterialMgrListener != NULL)
-    {			
-        Ogre::MaterialManager::getSingleton().removeListener(mMaterialMgrListener);
-        delete mMaterialMgrListener;
-        mMaterialMgrListener = NULL;
-    }
-    
-    // Destroy RTShader system.
-    if (mShaderGenerator != NULL)
-    {				
-        Ogre::RTShader::ShaderGenerator::destroy();
-        mShaderGenerator = NULL;
-    }
-}
-#endif // INCLUDE_RTSHADER_SYSTEM
 
 void Chess::startDemo()
 {
 	new OgreFramework();
-	if(!OgreFramework::getSingletonPtr()->initOgre("Chess", this, this))
+    if(!OgreFramework::getSingletonPtr()->initOgre("Chess", this, this)) {
 		return;
+    }
     
 	m_bShutdown = false;
     
 	OgreFramework::getSingletonPtr()->m_pLog->logMessage("Demo initialized!");
-	
-#ifdef INCLUDE_RTSHADER_SYSTEM
-    initialiseRTShaderSystem(OgreFramework::getSingletonPtr()->m_pSceneMgr);
-    Ogre::MaterialPtr baseWhite = Ogre::MaterialManager::getSingleton().getByName("BaseWhite", Ogre::ResourceGroupManager::INTERNAL_RESOURCE_GROUP_NAME);				
-    baseWhite->setLightingEnabled(false);
-    mShaderGenerator->createShaderBasedTechnique("BaseWhite", Ogre::MaterialManager::DEFAULT_SCHEME_NAME, Ogre::RTShader::ShaderGenerator::DEFAULT_SCHEME_NAME);
-    mShaderGenerator->validateMaterial(Ogre::RTShader::ShaderGenerator::DEFAULT_SCHEME_NAME, "BaseWhite");
-    baseWhite->getTechnique(0)->getPass(0)->setVertexProgram(baseWhite->getTechnique(1)->getPass(0)->getVertexProgram()->getName());
-    baseWhite->getTechnique(0)->getPass(0)->setFragmentProgram(baseWhite->getTechnique(1)->getPass(0)->getFragmentProgram()->getName());
-    
-    // creates shaders for base material BaseWhiteNoLighting using the RTSS
-    mShaderGenerator->createShaderBasedTechnique("BaseWhiteNoLighting", Ogre::MaterialManager::DEFAULT_SCHEME_NAME, Ogre::RTShader::ShaderGenerator::DEFAULT_SCHEME_NAME);
-    mShaderGenerator->validateMaterial(Ogre::RTShader::ShaderGenerator::DEFAULT_SCHEME_NAME, "BaseWhiteNoLighting");
-    Ogre::MaterialPtr baseWhiteNoLighting = Ogre::MaterialManager::getSingleton().getByName("BaseWhiteNoLighting", Ogre::ResourceGroupManager::INTERNAL_RESOURCE_GROUP_NAME);
-    baseWhiteNoLighting->getTechnique(0)->getPass(0)->setVertexProgram(baseWhiteNoLighting->getTechnique(1)->getPass(0)->getVertexProgram()->getName());
-    baseWhiteNoLighting->getTechnique(0)->getPass(0)->setFragmentProgram(baseWhiteNoLighting->getTechnique(1)->getPass(0)->getFragmentProgram()->getName());
-#endif
-    
+	  
 	setupChessScene();
-    
     
 #if !((OGRE_PLATFORM == OGRE_PLATFORM_APPLE) && __LP64__)
 	runDemo();
